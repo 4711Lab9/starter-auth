db3872ae3575470b34330bb0657de088c8112c60
Evgeni Maiseyeu <emaiseyeu@my.bcit.ca>
2016-03-29 14:37:08 -0700

    added access control

--- application/config/constants.php
+++ application/config/constants.php
@@ -9,6 +9,7 @@ define('XMLSUFFIX', '.xml');
 define('ROLE_USER','user');
 define('ROLE_ADMIN','admin');
 
+
 /*
   |--------------------------------------------------------------------------
   | File Stream Modes
